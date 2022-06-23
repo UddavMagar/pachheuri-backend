a46fb0b1d4b990a9950b8d732bab309bab4cdafd
UddavMagar <uddav.magar73@gmail.com>
2022-06-23 20:26:51 +0545

    inventory and discount added

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from product.models import Category
+from product.models import Category,Inventory,\
+    Discount
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Inventory)
+admin.site.register(Discount)
--- product/models.py
+++ product/models.py
@@ -1,4 +1,4 @@
-from unittest.util import _MAX_LENGTH
+
 from django.db import models
 
 # Create your models here.
@@ -14,5 +14,33 @@ class Category(models.Model):
         verbose_name_plural = "product_category"
         ordering = ["-created_at"]
 
+
+class Inventory(models.Model):
+    "Inventory model"
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __int__(self):
+        return self.quantity
+
+    class Meta:
+        verbose_name_plural = "product_inventory"
+        ordering = ["-created_at"]
+
+
+class Discount(models.Model):
+    name = models.CharField(max_length=100)
+    discount_percent = models.DecimalField(max_digits=8, decimal_places=2)
+    active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "product_discount"
+        ordering = ["-created_at"]
 
         
